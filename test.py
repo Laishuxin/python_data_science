c9079d214ead15b522ce6122ecf5cff645388556
Laishuxin <15622971239@163.com>
2019-11-09 16:12:27 +0800

    seaborn

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
+# 导入3d模块
+# 导入子模块后，想要在普通坐标轴加入projection='3d'就可以创建3维坐标轴
+from mpl_toolkits import mplot3d
+import numpy as np
 import matplotlib.pyplot as plt
-data = [1,2,3]
-plt.plot(data)
+plt.rcParams['figure.dpi'] = 200
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
 plt.show()
